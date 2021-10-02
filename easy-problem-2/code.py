1f6691d9ec36b198ec7f59b57f62c1ca4f883776
= <=>
2021-10-02 12:09:32 -0400

    more explanation

--- easy-problem-2/code.py
+++ easy-problem-2/code.py
@@ -1,19 +1,1 @@
-# "Shift Sift"
 
-arr = [i for i in range(10)]
-
-sum = 0
-
-right = True;
-while len(arr) > 0:
-    for i in arr:
-        sum += i
-        print(i)
-    if(right):
-        right = False
-        arr.pop(0);
-    else:
-        right = True
-        arr.pop()
-
-print("sum is: {}".format(sum))
---
+++ easy-problem-2/example.py
@@ -0,0 +1,31 @@
+# "Shift Sift"
+
+arr = [i for i in range(1,6)]
+
+sum = 0
+
+def print_pretty(arr):
+    print('[',end ="")
+    for i in arr:
+        if(arr.index(i) == len(arr)-1):
+            print("{}]".format(i))
+        else:
+            print(i,end = ",")
+        
+buffer = ""
+shift_right = True;
+while len(arr) > 0:
+    for i in arr:
+        sum += i
+    print(buffer,end="")
+    buffer +=" "
+    print_pretty(arr)
+    if(shift_right):
+        shift_right = False
+        arr.pop();
+    else:
+        shift_right = True
+        arr.pop(0)
+
+print('\n')
+print("sum is: {}".format(sum))
